0e1c68541941f8451d8ee864233358553c7f87e0
Jitendra Agarwal <>
2021-12-29 23:54:37 +0530

    instinal setup

---
+++ PujaBhandar/PujaBhandarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PujaBhandarApp.swift
+//  PujaBhandar
+//
+//  Created by Jitendra Agarwal on 29/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PujaBhandarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
